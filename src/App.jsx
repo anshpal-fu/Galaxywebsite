147fe8920b48f0836c9f39d6c3fa1f1e4e82638d
AnshPal <ansh.pal@lmsathena.com>
2025-12-22 04:37:19 -0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import Header from './components/Header.jsx'
--- src/components/CosmicServiceCard.jsx
+++ src/components/CosmicServiceCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const CosmicServiceCard = ({ title, description, features, link }) => {
   const [isHovered, setIsHovered] = useState(false);
