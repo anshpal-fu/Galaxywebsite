6bfb9fafb11379cbbf7eefe850b933772a2ae58a
AnshPal <ansh.pal@lmsathena.com>
2025-12-22 09:50:15 -0800

    y

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ icon, title, description, link, features }) => {
   return (
